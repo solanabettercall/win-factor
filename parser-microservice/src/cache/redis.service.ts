25f46180e33757bb4d07da3b10bcac37a38e16c5
EvgeniyLednev <lednev.fortech@gmail.com>
2025-05-04 01:38:22 +0300

    Добавлена поддержка сериализация/десериализация JSON в Redis с class-transformer

--- parser-microservice/src/cache/redis.service.ts
+++ parser-microservice/src/cache/redis.service.ts
@@ -1,12 +1,17 @@
 import {
   Injectable,
+  InternalServerErrorException,
   Logger,
   OnModuleDestroy,
   OnModuleInit,
 } from '@nestjs/common';
 import Redis from 'ioredis';
 import { appConfig } from 'src/config/parser.config';
-
+import {
+  ClassConstructor,
+  instanceToPlain,
+  plainToInstance,
+} from 'class-transformer';
 @Injectable()
 export class RedisService implements OnModuleInit, OnModuleDestroy {
   private client: Redis;
@@ -39,5 +44,60 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
 
   onModuleDestroy() {
     this.client?.disconnect();
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.client.get(key);
+  }
+
+  async set(key: string, value: string, ttl?: number): Promise<'OK'> {
+    if (ttl) {
+      return this.client.set(key, value, 'EX', ttl);
+    }
+    return this.client.set(key, value);
+  }
+
+  async del(key: string): Promise<number> {
+    return this.client.del(key);
+  }
+
+  async exists(key: string): Promise<boolean> {
+    const result = await this.client.exists(key);
+    return result === 1;
+  }
+
+  async getJson<T extends object>(
+    key: string,
+    cls: ClassConstructor<T>,
+  ): Promise<T | null> {
+    const result = await this.client.call('JSON.GET', key);
+    if (!result) return null;
+
+    const plain = JSON.parse(result as string);
+    return plainToInstance(cls, plain);
+  }
+
+  async setJson<T extends object>(
+    key: string,
+    value: T,
+    ttl?: number,
+  ): Promise<'OK' | Error> {
+    const plainObject = instanceToPlain(value);
+    const jsonString = JSON.stringify(plainObject);
+
+    const result = await this.client.call('JSON.SET', key, '.', jsonString);
+
+    if (ttl) {
+      await this.client.expire(key, ttl);
+    }
+
+    if (result === 'OK') {
+      return 'OK';
+    } else {
+      throw new InternalServerErrorException(
+        value,
+        'Ошибка при установке JSON',
+      );
+    }
   }
 }
--- parser-microservice/src/parser/parser.service.ts
+++ parser-microservice/src/parser/parser.service.ts
@@ -1,14 +1,12 @@
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import {
-  Injectable,
-  Logger,
-  OnApplicationBootstrap,
-  OnModuleInit,
-} from '@nestjs/common';
-import { VolleystationSocketService } from './sites/volleystation/volleystation-socket.service';
+  PlayByPlayEvent,
+  VolleystationSocketService,
+} from './sites/volleystation/volleystation-socket.service';
 import { VolleystationService } from './sites/volleystation/volleystation.service';
 import { firstValueFrom, retry } from 'rxjs';
 import { competitions } from './sites/volleystation/consts';
-import { isToday } from 'date-fns';
+import { RedisService } from 'src/cache/redis.service';
 
 @Injectable()
 export class ParserService implements OnApplicationBootstrap {
@@ -17,14 +15,21 @@ export class ParserService implements OnApplicationBootstrap {
   constructor(
     private readonly volleynetSocketService: VolleystationSocketService,
     private readonly volleynetService: VolleystationService,
+    private readonly redisService: RedisService,
   ) {}
 
   async onApplicationBootstrap() {
-    const match = await this.volleynetSocketService.getMatchInfo(2161023);
-    console.log(match);
+    const matchId = 2161023;
+    const key = `match:${matchId}`;
+    // const match = await this.volleynetSocketService.getMatchInfo(matchId);
+    // // console.log(match);
+    // await this.redisService.setJson<PlayByPlayEvent>(key, match, 360);
+
+    const cachedMatch = await this.redisService.getJson(key, PlayByPlayEvent);
+    console.log(cachedMatch);
+
     // console.log(`home: ${match.teams.home.name}`);
     // console.log(`away: ${match.teams.away.name}`);
-
     // const matches = await firstValueFrom(
     //   this.volleynetService.getMatches(
     //     competitions.find((c) => c.id === 320),
@@ -32,12 +37,10 @@ export class ParserService implements OnApplicationBootstrap {
     //   ),
     // );
     // console.log(matches);
-
     // for (const competition of competitions) {
     //   const matches = await firstValueFrom(
     //     this.volleynetService.getMatches(competition, 'schedule'),
     //   );
-
     //   this.logger.log(
     //     `[${competition.id}] ${competition.name} [${matches.length}]`,
     //   );
