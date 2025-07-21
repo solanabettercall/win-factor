e363c36b1329cce25e4fe75c87dbb8167df885d2
solanabettercall <solanabettercall@gmail.com>
2025-07-21 21:32:20 +0300

    Добавил таймаут для HTTP запросов

--- parser/src/parser/sites/volleystation/volleystation.module.ts
+++ parser/src/parser/sites/volleystation/volleystation.module.ts
@@ -14,6 +14,7 @@ import { VolleystationCacheService } from './volleystation-cache.service';
             'user-agent':
               'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0',
           },
+          timeout: 10000,
         };
         const config = appConfig();
         if (config.isLocal && config.proxy) {
